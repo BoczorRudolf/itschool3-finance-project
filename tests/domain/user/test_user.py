27a57b1fa9abf3ea82c848280254fcacab919623
Boczor_Al_Capone_Rudolf <boczor.rudolf@yahoo.com>
2023-05-09 16:30:38 +0300

    modified test user

--- tests/domain/user/test_user.py
+++ tests/domain/user/test_user.py
@@ -1,28 +1,38 @@
 import unittest
-
+import uuid
 from domain.user.user import User
 
 
 class UserMyTasteCase(unittest.TestCase):
     def test_user_sets_the_right_username(self):
         # set up
-        username = "random_generated"
-        user = User(username)
+        username = "random-generated"
+        id_ = uuid.uuid4()
+        user = User(id_, username)
         # execution
         actual_username = user.username
         # assertion
         self.assertEqual(username, actual_username)
 
     def test_it_sets_empty_list_if_we_do_not_specify_stock(self):
-        user = User("random-username")
+        id_ = uuid.uuid4()
+        username = "random-username"
+        user = User(id_, username)
 
         actual_stocks = user.stocks
 
         self.assertEqual([], actual_stocks)
 
-    @unittest.skip
     def test_it_sets_the_stocks_we_give(self):
-        pass
+        id_ = uuid.uuid4()
+        username = "random-name"
+        stock_list = ["first", "second", "third"]
+
+        user = User(id_, username, stock_list)
+
+        actual = user.stocks
+
+        self.assertEqual(stock_list, actual)
 
 
 if __name__ == "__main__":
