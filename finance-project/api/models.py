dab68b59c5de9c5fb8c4c084c49a96f06d87195e
Boczor_Al_Capone_Rudolf <boczor.rudolf@yahoo.com>
2023-05-10 12:14:34 +0300

    added today low/ high price, % diff between closed and current/ descriptions/ fixed av_today_price error

--- finance-project/api/models.py
+++ finance-project/api/models.py
@@ -3,37 +3,72 @@ from uuid import UUID
 
 
 class UserAdd(BaseModel):
-    username: str = Field(description="Alphanumeric username between 6 and 20 chars")
+    username: str = Field(
+        description="Alphanumeric username between 6 and 20 chars"
+    )
 
 
 
 
 class AssetAdd(BaseModel):
-    ticker: str
+    ticker: str = Field(
+        description="Unique series of letters that represent a company's stock or security"
+    )
 
 class AssetInfoBase(BaseModel):
-    ticker: str
-    name: str
-    country: str
+    ticker: str = Field(
+        description="Unique series of letters that represent a company's stock or security"
+    )
+    name: str = Field(
+        description="Name of a company based on the ticker given"
+    )
+    country: str = Field(
+        description="Country where a company is located based on the ticker given"
+                         )
 
     class Config:
         orm_mode = True
 
 class AssetInfoUser(AssetInfoBase):
-    units: float
+    units: float = Field(
+        description="Number of stocks or security a user has"
+    )
 
 class AssetInfoPrice(AssetInfoBase):
-    current_price: float
+    current_price: float = Field(
+        description="Current price of a stock or security"
+    )
     currency: str
-    #avg_today_price: float
-    #open_price: float
-    closed_price: float
-    fifty_day_price: float
+    open_price: float = Field(
+        description="Price at which a stock or security begins trading"
+                              )
+    avg_today_price: float = Field(
+        default=None, description="Average today price"
+    )
+    today_low_price: float = Field(
+        description="This refers to the minimum value at which a specific stock or security has been sold or exchanged."
+
+    )
+    today_high_price: float = Field(
+        description="This refers to the maximum price at which a stock or security was sold"
+
+    )
+    closed_price: float = Field(
+        description="Yesterday's last recorded price of a stock or security"
+                                )
+    fifty_day_price: float = Field(
+        description="Average price for fifty days"
+    )
+    percentage_difference_between_closed_and_current_price: str = Field(
+        description="Percentage difference between closed price and current price"
+    )
 
 class UserInfo(BaseModel):
-    id: UUID
+    id: UUID = Field(description="ID to identify a user")
     username: str
-    stocks: list[AssetInfoBase]
+    stocks: list[AssetInfoBase] = Field(
+        description="List of stocks that represent ownership in a publicly-traded company"
+                                        )
 
     class Config:
         allow_population_by_field_name = True
--- finance-project/domain/asset/asset.py
+++ finance-project/domain/asset/asset.py
@@ -1,46 +1,52 @@
 import yfinance
 
+
 class Asset:
     def __init__(self, ticker: str, nr: float, name: str, country: str, sector: str):
-        self.__ticker = ticker
-        self.__nr = nr
-        self.__name = name
-        self.__country = country
-        self.__sector = sector
-        yfin = yfinance.Ticker(ticker)
-        self.__info = yfin.fast_info
+        self.ticker = ticker
+        self.units = nr
+        self.name = name
+        self.country = country
+        self.sector = sector
+        self.info = yfinance.Ticker(ticker).fast_info
 
-    @property
-    def ticker(self) -> str:
-        return self.__ticker
-
-    @property
-    def units(self) -> float:
-        return self.__nr
-
-    @property
-    def name(self) -> str:
-        return self.__name
-
-    @property
-    def country(self) -> str:
-        return self.__country
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.ticker!r})"
 
     @property
     def current_price(self) -> float:
-       price = self.__info["lastPrice"]
-       return round(price, 2)
-
+        return round(self.info["lastPrice"], 2)
 
     @property
     def currency(self) -> str:
-        return self.__info["currency"]
+        return self.info["currency"]
+
+    @property
+    def today_low_price(self) -> float:
+        return self.info["dayLow"]
+
+    @property
+    def today_high_price(self) -> float:
+        return self.info["dayHigh"]
+
+    @property
+    def open_price(self) -> float:
+        return self.info["open"]
 
     @property
     def closed_price(self) -> float:
-        return self.__info["previousClose"]
-
+        return self.info["previousClose"]
 
     @property
     def fifty_day_price(self) -> float:
-        return self.__info["fiftyDayAverage"]
+        return self.info["fiftyDayAverage"]
+
+    @property
+    def percentage_difference_between_closed_and_current_price(self) -> str:
+        difference = self.closed_price - self.current_price
+        percentage_difference = (difference / self.closed_price) * 100
+        if difference > 0:
+            return f"The closed price {self.closed_price} is {percentage_difference:.2f}% higher than the current price {self.current_price}"
+        elif difference < 0:
+            return f"The closed {self.closed_price} is {abs(percentage_difference):.2f}% lower than current {self.current_price}"
+        return "The values are the same"
