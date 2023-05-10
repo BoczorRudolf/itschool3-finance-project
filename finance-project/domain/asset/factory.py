cd81c95d74c6a473e54789c6744a32d37cc3d6d6
Boczor_Al_Capone_Rudolf <boczor.rudolf@yahoo.com>
2023-05-10 15:02:40 +0300

    updated factory/ created test_factory for asset

--- finance-project/domain/asset/factory.py
+++ finance-project/domain/asset/factory.py
@@ -1,26 +1,36 @@
 import yahooquery
 from domain.asset.asset import Asset
+from persistence.exceptions import InvalidTicker
 
 
 class AssetFactory:
-    def make_new(self, ticker: str) -> Asset:
-        t = yahooquery.Ticker(ticker)
-        profile = t.summary_profile[ticker]
-        name = self.__extract_name(profile)
-        country = profile["country"]
-        sector = profile["sector"]
-        return Asset(
-            ticker=ticker,
-            nr=0,
-            name=name,
-            country=country,
-            sector=sector,
-        )
 
-    def __extract_name(self, profile: dict) -> str:
+    @staticmethod
+    def make_new(ticker: str) -> Asset:
+        try:
+            t = yahooquery.Ticker(ticker)
+            profile = t.summary_profile[ticker]
+            if type(profile) not in [dict]:
+                raise InvalidTicker(f"Invalid ticker {ticker}")
+            name = AssetFactory.__extract_name(profile)
+            country = profile["country"]
+            sector = profile["sector"]
+            return Asset(
+                ticker=ticker,
+                nr=0,
+                name=name,
+                country=country,
+                sector=sector,
+            )
+        except InvalidTicker as e:
+            raise e
+        except Exception as e:
+            raise ValueError(f"Could not create asset with ticker {ticker}") from e
+
+    @staticmethod
+    def __extract_name(profile: dict) -> str:
         summary = profile["longBusinessSummary"]
         words = summary.split(" ")
         first_2_words = words[0:2]
         name = " ".join(first_2_words)
         return name
-
