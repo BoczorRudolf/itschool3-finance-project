85b56df29bdf0ac2afe5e2a303e501fc6525f837
Boczor_Al_Capone_Rudolf <boczor.rudolf@yahoo.com>
2023-05-10 12:28:40 +0300

    no more duplicate code, orm_mode = True

--- finance-project/api/models.py
+++ finance-project/api/models.py
@@ -2,20 +2,24 @@ from pydantic import BaseModel, Field
 from uuid import UUID
 
 
-class UserAdd(BaseModel):
+class OrmModel(BaseModel):
+    class Config:
+        orm_mode = True
+
+
+class UserAdd(OrmModel):
     username: str = Field(
         description="Alphanumeric username between 6 and 20 chars"
     )
 
 
-
-
-class AssetAdd(BaseModel):
+class AssetAdd(OrmModel):
     ticker: str = Field(
         description="Unique series of letters that represent a company's stock or security"
     )
 
-class AssetInfoBase(BaseModel):
+
+class AssetInfoBase(OrmModel):
     ticker: str = Field(
         description="Unique series of letters that represent a company's stock or security"
     )
@@ -24,15 +28,14 @@ class AssetInfoBase(BaseModel):
     )
     country: str = Field(
         description="Country where a company is located based on the ticker given"
-                         )
+    )
 
-    class Config:
-        orm_mode = True
 
 class AssetInfoUser(AssetInfoBase):
     units: float = Field(
         description="Number of stocks or security a user has"
     )
+
 
 class AssetInfoPrice(AssetInfoBase):
     current_price: float = Field(
@@ -41,21 +44,19 @@ class AssetInfoPrice(AssetInfoBase):
     currency: str
     open_price: float = Field(
         description="Price at which a stock or security begins trading"
-                              )
+    )
     avg_today_price: float = Field(
         default=None, description="Average today price"
     )
     today_low_price: float = Field(
         description="This refers to the minimum value at which a specific stock or security has been sold or exchanged."
-
     )
     today_high_price: float = Field(
         description="This refers to the maximum price at which a stock or security was sold"
-
     )
     closed_price: float = Field(
         description="Yesterday's last recorded price of a stock or security"
-                                )
+    )
     fifty_day_price: float = Field(
         description="Average price for fifty days"
     )
@@ -63,13 +64,10 @@ class AssetInfoPrice(AssetInfoBase):
         description="Percentage difference between closed price and current price"
     )
 
-class UserInfo(BaseModel):
+
+class UserInfo(OrmModel):
     id: UUID = Field(description="ID to identify a user")
     username: str
     stocks: list[AssetInfoBase] = Field(
         description="List of stocks that represent ownership in a publicly-traded company"
-                                        )
-
-    class Config:
-        allow_population_by_field_name = True
-        orm_mode = True
+    )
