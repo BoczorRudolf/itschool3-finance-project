eaf9c74a3f765e3276624d58ca604d2de13eff7d
Boczor_Al_Capone_Rudolf <boczor.rudolf@yahoo.com>
2023-05-11 17:48:46 +0300

    added @app.exception_handlers

--- finance-project/main.py
+++ finance-project/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi_utils.tasks import repeat_every
 from api.users import users_router
 from api.assets import assets_router
+from domain.exceptions import NonExistentUserId, InvalidTicker, DuplicateUser, DuplicateAsset
 from domain.user.factory import InvalidUsername
 from starlette.responses import JSONResponse
 import subprocess
@@ -28,9 +29,38 @@ app.include_router(users_router)
 app.include_router(assets_router)
 
 
+@app.exception_handler(DuplicateAsset)
+def return_duplicate_asset(_: Request, e: DuplicateAsset):
+    logging.error(str(e))
+    return JSONResponse(status_code=409, content=str(e))
+
+
+@app.exception_handler(DuplicateUser)
+def return_duplicate_user(_: Request, e: DuplicateUser):
+    logging.warning(str(e))
+    return JSONResponse(status_code=409, content=str(e))
+
+
 @app.exception_handler(InvalidUsername)
 def return_invalid_username(_: Request, e: InvalidUsername):
-    return JSONResponse(status_code=400, content="Username is not valid! Error: " + str(e))
+    invalid_username_error = "Username is not valid! Error: " + str(e)
+    logging.error(invalid_username_error)
+    return JSONResponse(status_code=400, content=invalid_username_error)
+
+
+@app.exception_handler(NonExistentUserId)
+def return_invalid_id(_: Request, e: NonExistentUserId):
+    invalid_id_error = str(e)
+    logging.error(invalid_id_error)
+    return JSONResponse(status_code=404, content=invalid_id_error)
+
+
+@app.exception_handler(InvalidTicker)
+def return_invalid_ticker(_: Request, e: InvalidTicker):
+    logging.error(str(e))
+    return JSONResponse(status_code=404, content=str(e))
+
+
 
 @app.on_event("startup")
 @repeat_every(seconds=60 * 60 * 24)
